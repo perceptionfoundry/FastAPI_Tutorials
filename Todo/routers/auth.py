db208c4f904791a227c6365d817360161872d1c5
Perception Tech <31928839+perceptionfoundry@users.noreply.github.com>
2025-01-08 15:11:41 +0500

    auth

--- Todo/routers/auth.py
+++ Todo/routers/auth.py
@@ -1,9 +1,12 @@
-from fastapi import APIRouter
+from typing import Annotated
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
 from pydantic import BaseModel
-from sqlalchemy.util import deprecated
-
+from database import SessionLocal
 from models import Users
 from passlib.context import CryptContext
+from starlette import status
+from fastapi.security import OAuth2PasswordRequestForm
 
 router = APIRouter()
 
@@ -17,12 +20,32 @@ class CreateUserRequest(BaseModel):
     password : str
     role : str
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
+db_dependency = Annotated[Session, Depends(get_db)]
 
+#Auth processing
+def authenticate_user(username:str,
+                      password:str,
+                      db):
+    user = db.query(Users).filter(Users.username == username).first()
 
-@router.post('/auth/')
-async def create_user(create_request : CreateUserRequest):
+    if not user:
+        return False
+    if not bcrypt_context.verify(password,user.hashed_password):
+        return False
+    return  True
+
+#******* Create User
+@router.post('/auth/', status_code = status.HTTP_201_CREATED)
+async def create_user(db: db_dependency,
+                      create_request : CreateUserRequest):
 
     create_user_model = Users(
         email = create_request.email,
@@ -34,4 +57,18 @@ async def create_user(create_request : CreateUserRequest):
         is_active = True
     )
 
-    return create_user_model
+    db.add(create_user_model)
+    db.commit()
+
+#************** Create Token on Access
+@router.post('/tokens')
+async def login_for_access_token(form_data:Annotated[OAuth2PasswordRequestForm,Depends()],
+                                 db: db_dependency):
+
+    user = authenticate_user(form_data.username,
+                             form_data.password,
+                             db)
+
+    if not user:
+        return "failed auth"
+    return "success auth"
