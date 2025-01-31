6810d3ebce117cc74a7274cd6cd954f213751b9a
Perception Tech <31928839+perceptionfoundry@users.noreply.github.com>
2025-01-31 09:52:12 +0500

    new push

--- Todo/main.py
+++ Todo/main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 
 import models
 from database import engine
-from routers import auth, todo, admin
+from routers import auth, todo, admin, Users
 
 
 app = FastAPI()
@@ -13,5 +13,6 @@ models.Base.metadata.create_all(bind=engine)
 app.include_router(auth.router)
 app.include_router(todo.router)
 app.include_router(admin.router)
+app.include_router(Users.router)
 
 
